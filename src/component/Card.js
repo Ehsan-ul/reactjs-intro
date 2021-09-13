26c7cee720e6de9fab59d2e36a9141704da695b5
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-09-13 18:08:21 +0600

    first commit

---
+++ src/component/Card.js
@@ -0,0 +1,20 @@
+// const h1Style = {
+//   backgroundColor: 'black',
+//   padding: '10px',
+//   color: 'white',
+//   textAlign: 'center',
+// };
+
+// const myName = 'Ehsanul Islam';
+
+function Card(props) {
+  return (
+    <div className='card'>
+      <h2 style={{ color: 'red' }}>Hello World!</h2>
+      <p className='p1'>{`My name is ${myName}`}</p>
+      <p className='p2'> This is paragraph 2</p>
+    </div>
+  );
+}
+
+export default Card;
